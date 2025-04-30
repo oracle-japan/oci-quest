b4c56cf15368dff701f22b9bf7df9ca9e963dfaa
sogawa-yk <daybreaks.link@gmail.com>
2025-04-30 15:34:11 +0900

    前提環境作成

---
+++ terraform/quest/log_analytics.tf
@@ -0,0 +1,11 @@
+
+
+
+
+
+resource "oci_log_analytics_namespace" "test_namespace" {
+    #Required
+    compartment_id = var.tenancy_ocid
+    is_onboarded = true
+    namespace = "ociquestkddi"
+}
