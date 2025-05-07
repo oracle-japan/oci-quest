06be6db84611584be0dedaee025c8079cbe274e5
sogawa-yk <daybreaks.link@gmail.com>
2025-05-07 09:50:07 +0900

    fix

--- terraform/quest/log_analytics.tf
+++ terraform/quest/log_analytics.tf
@@ -3,9 +3,9 @@
 
 
 
-resource "oci_log_analytics_namespace" "test_namespace" {
-    #Required
-    compartment_id = var.tenancy_ocid
-    is_onboarded = true
-    namespace = "ociquestkddi"
-}
+# resource "oci_log_analytics_namespace" "test_namespace" {
+#     #Required
+#     compartment_id = var.tenancy_ocid
+#     is_onboarded = true
+#     namespace = "ociquestkddi"
+# }
