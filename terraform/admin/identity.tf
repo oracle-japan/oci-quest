43c7755927257d24138dac4c4d2d759bb68bbae7
sogawa-yk <daybreaks.link@gmail.com>
2025-05-09 14:31:20 +0900

    devコンパートメント追加

--- terraform/admin/identity.tf
+++ terraform/admin/identity.tf
@@ -26,6 +26,7 @@ locals {
     }
 }
 
+
 resource "oci_identity_compartment" "teams" {
     for_each = local.team_map
 
@@ -33,6 +34,13 @@ resource "oci_identity_compartment" "teams" {
     description    = each.value.description
     compartment_id = var.tenancy_ocid 
     enable_delete  = true              
+}
+
+resource "oci_identity_compartment" "admin_dev" {
+    name = "admin_dev"
+    description = "運営用のコンパートメントです。"
+    compartment_id = var.tenancy_ocid
+    enable_delete = true
 }
 
 resource "oci_identity_user" "users" {
@@ -52,11 +60,25 @@ resource "oci_identity_group" "teams" {
   compartment_id = var.tenancy_ocid
 }
 
+resource "oci_identity_group" "admin_dev" {
+  name = "admin_dev"
+  description = "運営用"
+  compartment_id = var.tenancy_ocid
+}
+
 resource "oci_identity_user_group_membership" "test_user_group_membership" {
   for_each = local.member_map
   user_id = oci_identity_user.users[each.key].id
   group_id = oci_identity_group.teams[each.value.team_name].id
 }
+
+resource "oci_identity_user_group_membership" "admin_dev_membership" {
+  for_each = { for id in var.admin_user_ocids : id => id }
+
+  user_id  = each.key
+  group_id = oci_identity_group.dev.id
+}
+
 
 resource "oci_identity_policy" "team_access" {
   for_each = local.team_map
@@ -73,6 +95,22 @@ resource "oci_identity_policy" "team_access" {
 
   depends_on = [null_resource.wait_for_compartments]
 }
+
+resource "oci_identity_policy" "dev_admin_access" {
+  name           = "dev_admin_access_policy"
+  description    = "devグループにdevコンパートメントの管理権限を付与"
+  compartment_id = var.tenancy_ocid
+
+  statements = [
+    "Allow group dev to manage all-resources in compartment dev",
+    "Allow service dpd to manage objects in compartment dev",
+    "Allow service dpd to read secret-family in compartment dev"
+  ]
+
+  depends_on = [oci_identity_compartment.dev]
+}
+
+
 
 # コンパートメント作成直後にはポリシーを作成できないので、待つ必要がある
 resource "null_resource" "wait_for_compartments" {
