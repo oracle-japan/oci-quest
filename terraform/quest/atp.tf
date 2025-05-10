b1af9b356984d0249f8c32319bb77977f5bab16c
sogawa-yk <daybreaks.link@gmail.com>
2025-05-10 14:27:31 +0900

    fix

--- terraform/quest/atp.tf
+++ terraform/quest/atp.tf
@@ -92,9 +92,7 @@ resource "oci_identity_policy" "dbm_secrets_policy" {
   name           = "dbm-secret-access"
   compartment_id = var.tenancy_ocid
   statements = [
-    "Allow service databasemanagement to read secret-bundles in compartment ${var.compartment_ocid}",
-    "Allow service databasemanagement to use vaults in compartment ${var.compartment_ocid}",
-    "Allow service databasemanagement to use keys in compartment ${var.compartment_ocid}"
+    "Allow service read to read vaults in compartment ${var.compartment_ocid}"
   ]
   description = "Policy to allow DB Management to access secrets"
 }
