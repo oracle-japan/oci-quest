4e3d36c97931652de9788acfd28ad8898f59d315
sogawa-yk <daybreaks.link@gmail.com>
2025-05-11 11:10:43 +0900

    fix

--- terraform/quest/log_analytics.tf
+++ terraform/quest/log_analytics.tf
@@ -1,11 +1,13 @@
+resource "oci_log_analytics_namespace" "test_namespace" {
+    #Required
+    compartment_id = var.tenancy_ocid
+    is_onboarded = true
+    namespace = "ociquestkddi"
+}
 
-
-
-
-
-# resource "oci_log_analytics_namespace" "test_namespace" {
-#     #Required
-#     compartment_id = var.tenancy_ocid
-#     is_onboarded = true
-#     namespace = "ociquestkddi"
-# }
+resource "oci_log_analytics_log_analytics_log_group" "la_group" {
+  compartment_id = var.compartment_ocid
+  display_name   = "example-log-group"
+  namespace = "ociquestkddi"  # 例: "frankfurtnamespace"
+  description    = "Log group for application logs"
+}
