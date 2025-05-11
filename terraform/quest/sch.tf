c7377e020e5acad38bb4be6b6d247b674ccd1131
sogawa-yk <daybreaks.link@gmail.com>
2025-05-11 21:36:11 +0900

    policy追加

--- terraform/quest/sch.tf
+++ terraform/quest/sch.tf
@@ -15,3 +15,13 @@ resource oci_sch_service_connector "audit_to_la" {
     log_group_id = oci_log_analytics_log_analytics_log_group.la_group.id
   }
 }
+
+resource "oci_identity_policy" "sch_policy" {
+  name           = "sch_policy"
+  compartment_id = var.tenancy_ocid
+  description = "コネクタハブのポリシー"
+  statements = [
+    "allow any-user to {LOG_ANALYTICS_LOG_GROUP_UPLOAD_LOGS} in compartment id ${oci_log_analytics_log_analytics_log_group.la_group.compartment_id} where all {request.principal.type = 'serviceconnector', target.loganalytics-log-group.id = '${oci_log_analytics_log_analytics_log_group.la_group.id}', request.principal.compartment.id = '${oci_sch_service_connector.audit_to_la.compartment_id}'}"
+  ]
+
+}
