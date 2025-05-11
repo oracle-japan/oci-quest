2e2a8ec9a6bb179933983dcc57ced7d227809495
sogawa-yk <daybreaks.link@gmail.com>
2025-05-11 21:45:14 +0900

    fix

--- terraform/quest/log_analytics.tf
+++ terraform/quest/log_analytics.tf
@@ -1,8 +1,9 @@
-resource "oci_log_analytics_namespace" "la_namespace" {
-  compartment_id = var.compartment_ocid
-  is_onboarded = true
-  namespace = "nrubtr8vonph"
-}
+# 一回有効化したら以降こいつがいるとエラーになるので、コメントアウトしておく
+# resource "oci_log_analytics_namespace" "la_namespace" {
+#   compartment_id = var.compartment_ocid
+#   is_onboarded = true
+#   namespace = "nrubtr8vonph"
+# }
 
 resource "oci_log_analytics_log_analytics_log_group" "la_group" {
   compartment_id = var.compartment_ocid
