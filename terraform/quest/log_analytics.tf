87e34564ac5c67313021b1b8eb05efde57f8fc5d
sogawa-yk <daybreaks.link@gmail.com>
2025-05-11 11:41:17 +0900

    fix

--- terraform/quest/log_analytics.tf
+++ terraform/quest/log_analytics.tf
@@ -1,13 +1,21 @@
+data "oci_log_analytics_namespaces" "default" {
+  compartment_id = var.tenancy_ocid
+}
+
+locals {
+  log_analytics_namespace = data.oci_log_analytics_namespaces.default.namespaces[0].name
+}
+
 resource "oci_log_analytics_namespace" "test_namespace" {
     #Required
     compartment_id = var.tenancy_ocid
     is_onboarded = true
-    namespace = "ociquestkddi"
+    namespace = local.log_analytics_namespace
 }
 
 resource "oci_log_analytics_log_analytics_log_group" "la_group" {
   compartment_id = var.compartment_ocid
   display_name   = "example-log-group"
-  namespace = "ociquestkddi"  # 例: "frankfurtnamespace"
+  namespace = local.log_analytics_namespace
   description    = "Log group for application logs"
 }
