dba2f7777791013b9f33a192672a74c82c66036e
sogawa-yk <daybreaks.link@gmail.com>
2025-05-12 09:38:55 +0900

    fix

--- terraform/quest/outputs.tf
+++ terraform/quest/outputs.tf
@@ -10,6 +10,17 @@ output "app_private_ip" {
   value = oci_core_instance.mushop_app_instance.private_ip
 }
 
-output "atp_connection_string" {
-  value = oci_database_autonomous_database.mushop_atp.connection_strings
+locals {
+  # 全プロファイルの中から HIGH の最初の value を見つける
+  high_value_string = one([
+    for profile in oci_database_autonomous_database.mushop_atp.connection_strings[0].profiles :
+    profile.value if profile.consumer_group == "HIGH"
+  ])
+
+  # high_value_string から host=... の部分だけを抽出する（正規表現）
+  high_host = regex("host=([a-zA-Z0-9.-]+)", local.high_value_string)[0]
+}
+
+output "high_host" {
+  value = local.high_host
 }
