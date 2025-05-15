7307dd768d4ef6c7444fe57b9b550a09e700450f
sogawa-yk <daybreaks.link@gmail.com>
2025-05-15 15:23:04 +0900

    LA無効化、namespaceをデータソースから取得

--- terraform/quest/log_analytics.tf
+++ terraform/quest/log_analytics.tf
@@ -1,13 +1,19 @@
+data "oci_objectstorage_namespace" "ns" {
+
+    #Optional
+    compartment_id = var.compartment_ocid
+}
+
 # 一回有効化したら以降こいつがいるとエラーになるので、コメントアウトしておく
-# resource "oci_log_analytics_namespace" "la_namespace" {
-#   compartment_id = var.compartment_ocid
-#   is_onboarded = true
-#   namespace = "nrubtr8vonph"
-# }
+resource "oci_log_analytics_namespace" "la_namespace" {
+  compartment_id = var.compartment_ocid
+  is_onboarded = false
+  namespace = data.oci_obejectstorage_namespace.ns.namespace
+}
 
 resource "oci_log_analytics_log_analytics_log_group" "la_group" {
   compartment_id = var.compartment_ocid
   display_name   = format("%s-la-log-group", var.team_name)
-  namespace = "nrubtr8vonph"
+  namespace = data.oci_obejectstorage_namespace.ns.namespace
   description    = "Log group for la logs"
 }
